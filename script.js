96cd3851475491785af89045cca3a82d775146a5
Cristian Lopez <66136708+KrzzLpzz@users.noreply.github.com>
2023-03-09 07:53:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function initServerData(serverIp, serverPort) {
+  const serverIpElement = document.getElementById('server-ip');
+  //serverIpElement.innerHTML = serverIp;
+
+  console.log('https://mcapi.us/server/status?ip=' + serverIp + '&port=' + serverPort);
+  fetch('https://mcapi.us/server/status?ip=' + serverIp + '&port=' + serverPort)
+    .then(response => response.json())
+    .then(data => handleServerStatus(data));
+
+  function handleServerStatus(data) {
+    if (data.status == 'error') {
+      console.log(data.error);
+      const serverError = document.getElementById('rest');
+      serverError.innerHTML = "TegusCraft está Offline";
+      return false;
+    }
+
+    const logo = document.getElementById("favicon");
+    logo.src = data.favicon;
+
+    const serverSuccess = document.getElementById('rest');
+    serverSuccess.innerHTML = '<b>IP del Servidor: </b>' + serverIp + '<br>' + '<b>Puerto: </b>' + serverPort + '<br>' + '<b>Jugadores Conectados: </b>' + data.players.now + '<br>' + '<b>MOTD: </b>' + data.motd + '<br><br>Agrega el servidor a Bedrock:'
+  }
+
+}
+
+initServerData("play.lospanasjeje.ml", "10022");
